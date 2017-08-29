e94404a37b0434337ef1ace1270f6fb5970e0e79
Radian <craziestdavid@gmail.com>
2017-08-30 01:49:47 +0800

    interface fin

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 
-var action
+app.use("/", express.static(__dirname + "public"));
+
+
+var action = "Idle"
 app.get('/set', function (req, res) {
   console.log("set action : " + action)
-  action = req.params.action + ""
+  action = req.params.action + "";
+
+  res.set('Content-Type', 'text/html')
+  res.send(action)
+
 })
 app.get('/action',function (req,res){
   console.log("sent action : " + action)
@@ -14,5 +21,6 @@ app.get('/action',function (req,res){
 
 
 app.listen(3000, function () {
+  console.log("static folder : " + __dirname + "public" );
   console.log('Example app listening on port 3000!')
 })
